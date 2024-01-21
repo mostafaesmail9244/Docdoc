a635a55fa109eca969c5c0f658d1d543c74068d6
Mostafa_Esmail <mostafaesmail9244@gmail.com>
2024-01-21 18:05:17 +0200

    add   await ScreenUtil.ensureScreenSize(); line

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mide_mate/bloc_observer.dart';
 import 'package:mide_mate/core/d_i/injection.dart';
 import 'package:mide_mate/core/routing/app_router.dart';
 import 'package:mide_mate/doc_app.dart';
 
-void main() {
+void main() async {
   setupGetIt();
   Bloc.observer = MyBlocObserver();
-
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
